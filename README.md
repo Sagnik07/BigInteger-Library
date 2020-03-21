815ef4e792f249fe5ce6d22ec523e58b58e141ea
Sagnik07 <sagnikgupta07@gmail.com>
2020-03-21 10:28:00 +0530

    problem statements added

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # BigInteger-Library
+
+Problem Statement: ​ Your task is to create a large integer library, similar to what
+we have in Java as BigInteger. 
+
+Your library should provide functionalities to store arbitrarily large integer and perform basic math operations.
+
+
+Operations to be implemented​ : 
+
+Fast exponentiation
+
+GCD of 2 integers
+
+Factorial of an integer.
--- bigint.cpp
+++ bigint.cpp
@@ -1,3 +1,12 @@
+/*
+Statement: ​ Your task is to create a large integer library, similar to what
+we have in Java as BigInteger. Your library should provide
+functionalities to store arbitrarily large integer and perform basic math
+operations.
+Operations to be implemented​ : Fast exponentiation, GCD of 2
+integers and factorial.
+*/
+
 #include<bits/stdc++.h>
 using namespace std;
 #define ll long long int
